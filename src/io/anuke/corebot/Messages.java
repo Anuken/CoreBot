7a44e41b7afc7579cc4b0bb4d1563d2fa3a67bbd
Milinai <41624505+Milinai@users.noreply.github.com>
2018-11-30 22:43:26 +0100

    Update Messages.java
    Added time zone info to avoid confusion and questions. (Hopefully 😜)

--- src/io/anuke/corebot/Messages.java
+++ src/io/anuke/corebot/Messages.java
@@ -70,7 +70,7 @@ public class Messages {
 
                 StringBuilder builder = new StringBuilder();
 
-                builder.append(Strings.formatArgs("*Last Updated: {0}*\n\n", DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss").format(LocalDateTime.now())));
+                builder.append(Strings.formatArgs("*Last Updated: {0} UTC*\n\n", DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss XXXX").format(LocalDateTime.now())));
 
                 //send new messages
                 for(PingResult result : results){
