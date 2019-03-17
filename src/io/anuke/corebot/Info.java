b1234a87114e7e101692ff12d5e26f22bec0cc24
Anuken <arnukren@gmail.com>
2019-03-17 11:56:48 -0400

    Better info

--- src/io/anuke/corebot/Info.java
+++ src/io/anuke/corebot/Info.java
@@ -15,14 +15,15 @@ public enum Info{
     "[Direct Link](https://play.google.com/apps/testing/io.anuke.mindustry)"),
     rules("Rules",
     "**1.** Don't be rude. This should be obvious. No racism/sexism/etc.\n" +
-    "**2.** No spamming or advertising. Don't post discord links unless someone asks for them.\n" +
+    "**2.** No spamming or advertising. Discord links are automatically deleted by the bot; if you want to invite someone to a server, do so in PM.\n" +
     "**3.** No NSFW content or politics. This includes NSFW conversations. Take it elsewhere.\n" +
     "**4.** Keep content to the aproppriate text channels.\n" +
-    "**5.** Please keep memes to a minimum.\n" +
-    "**6.** Please do not post invite links to this server in public places.\n" +
-    "**7.** Do not beg for roles.\n" +
+    "**5.** Please keep memes to a minimum; don't dump memes. Mindustry-related memes are fine.\n" +
+    "**6.** Please do not post invite links to this server in public places without context. If they have the game, they should be able to join anyway.\n" +
+    "**7.** Do not ask for roles. If I need a moderator and I think you fit the position, I will ask you personally.\n" +
     "**8.** Do not impersonate other members or intentionally edit your messages to mislead others.\n" +
-    "**9.** Breaking any of these rules in a non-serious way may result in a warning. 3 warnings is a ban.\n" +
+    "**9.** Do not cross-post the same message to multiple channels. One channel is enough.\n" +
+    "**10.** Breaking any of these rules in a non-serious way may result in a warning. 3 warnings is a ban.\n" +
     "*If I don't like your behavior, you're out. Obey the spirit, not the word.*"),
     multiplayer("Multiplayer",
     "Multiplayer in Mindustry works just as it does in many other sandbox games.\n" +
@@ -39,7 +40,7 @@ public enum Info{
     "Type `help` for a list of all commands.\n"),
     suggested("Suggested Ideas",
     "*The following ideas have been suggested many times before. Please stop.*\n\n" +
-    "- Steam release *(yes, it is planned)*\n" +
+    "- Steam/switch/console/etc release *(yes, it is planned, check the trello)*\n" +
     "- Map browser *(coming with steam workshop)*\n" +
     "- Easier multiplayer *(coming with steam)*\n" +
     "- Server browser *(possibly coming with steam)*\n" +
@@ -48,20 +49,19 @@ public enum Info{
     "- Modding support *(see the #mods channel)*\n" +
     "- Game speed increase *(not technically viable or needed)*\n" +
     "- Liquid teleporter *(overpowered, use phase conduits)*\n" +
-    "- Teleporters in 4.0 *(may or may not be coming eventually)*\n" +
     "- Power wires or conduits *(as opposed to nodes)*\n" +
     "- Underground map layer *(unnecessarily complex, use bridges)*\n" +
     "- Trains ***(no thanks)***\n" +
     "- \"hey I have a game idea you should make\" *(no comment)*"),
     pings("Ping and PM Policy",
     "*Please do not PM or ping me (Anuke) unless it is something important.*\n\n" +
-    "'Important' includes things like major unreported game server issues or serious exploits, " +
+    "'Important' includes things like major unreported game server issues or exploits, " +
     "problems with the Discord server (e.g. spammers), or Github pull request related issues." +
     "\n" +
-    "Do *not* PM or ping me when posting normal suggestions or bugs reports. If it's relevant, I'll read it.\nIf you have a question about the game, *ask in #general.*\n" +
+    "Do *not* PM or ping me when posting normal suggestions or bugs reports. If it's relevant, I'll read it.\nIf you have a question about the game, *ask in #general or #help.*\n" +
     "\n" +
-    "I will not respond to PMs with non-personal game-related questions or messages such as 'hey'.\n" +
-    "**I also do not accept random friend requests.**"),
+    "I will not respond to PMs with non-personal game-related questions or random messages such as 'hey'. And no, video calling me multiple times in PM doesn't get my attention, it just gets you blocked. *(Believe me, people have tried.)*\n" +
+    "*I also do not accept random friend requests from people I don't know.*"),
     bugs("Bug Reporting Info",
     "**Template:**\n" +
     "**Platform:** *<Android/iOS/Mac/Windows/Linux/Web>*\n" +
@@ -70,7 +70,7 @@ public enum Info{
     "**Status:**\n" +
     ":white_check_mark: - *fixed*\n" +
     ":question: - *can't reproduce the bug, additional info needed*\n" +
-    ":x: - *not a bug, intentional behavior*\n" +
+    ":x: - *not a bug, intentional behavior or beyond my control*\n" +
     ":fire: - *please stop reporting this*");
     public final String text;
     public final String title;
