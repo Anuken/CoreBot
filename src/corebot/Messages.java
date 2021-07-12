93ea6e743c6956413c6056a27867076efa6fcd27
Anuken <arnukren@gmail.com>
2021-07-12 10:12:00 -0400

    regex cleanup

--- src/corebot/Messages.java
+++ src/corebot/Messages.java
@@ -43,7 +43,7 @@ public class Messages extends ListenerAdapter{
     private static final SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
     private static final String[] warningStrings = {"once", "twice", "thrice", "too many times"};
     private static final Pattern invitePattern = Pattern.compile("(discord\\.gg/\\w|discordapp\\.com/invite/\\w|discord\\.com/invite/\\w)");
-    private static final Pattern scamPattern = Pattern.compile("stea.?.?com.*?\\.ru|CSGO.*knife|CSGO.*inventory");
+    private static final Pattern scamPattern = Pattern.compile("stea.*com.*\\.ru|CSGO.*knife|CSGO.*inventory");
 
     private final CommandHandler handler = new CommandHandler(prefix);
     private final CommandHandler adminHandler = new CommandHandler(prefix);
